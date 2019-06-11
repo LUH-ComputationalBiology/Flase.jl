67a039005220b587ecd835051791154a7f671633
Simon Christ <SimonChrist@gmx.de>
2019-06-11 19:04:49 +0200

    adjust parameters in running script to "production" values.

--- scripts/movingDogs_staticSheeps.jl
+++ scripts/movingDogs_staticSheeps.jl
@@ -1,23 +1,34 @@
 using Flase
 
+v0 = 5.0
+Dϕ = 4.0
 world = World(
     v0 = 1.,
-    n_dogs = 500,
-    boxsize = 10.0,
-    freedom_rate = 0.1,
+    n_dogs = 100,
+    boxsize = 250.0,
+    freedom_rate = 10.0,
+    business_rate = 10.0,
+    meanSleepyness = 10.0,
     motion = BrownianMotion(
-        noise = 0.5,
-        friction = 1.0
+        noise = v0^4 / Dϕ,
+        friction = Dϕ / v0^2
         ),
     sheeps = DenseSheeps(
-        n_sheeps = 25,
-        gridsize = 10,
+        n_sheeps = 200,
+        gridsize = 250,
         )
     )
 simulation = FiniteSimulation(;
-    dt = 0.2,
-    end_time = 200.0,
+    dt = 0.1,
+    end_time = 1000.0,
     world = world,
-    plotter = UnicodePlotter()
+    plotter = UnicodePlotter( skip = 200 )
     )
+    # TODO: get this smooth
+# simulation = FiniteSimulation(;
+#     dt = 0.1,
+#     end_time = 20_000.0,
+#     world = world,
+#     plotter = UnicodePlotter( skip = 2_000 )
+#     )
 Flase.runsim( simulation )
