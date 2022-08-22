4ee2aa81ebaf511879b1dce20924a8520c61053e
Simon Christ <simonchrist@gmx.de>
2022-08-22 12:46:05 +0200

    move center_of_mass

--- src/items/Sheeps.jl
+++ src/items/Sheeps.jl
@@ -51,38 +51,3 @@ function diffuseSheep!( sheep::Sheeps{L}, i, j ) where L
     setNSheep!( sheep, i, j, new_sheep + 1 )
     return nothing
 end # function
-
-function center_of_mass( sheeps::Sheeps{L}, world::World) where L
-    r = L / 2pi
-    xt1 = 0
-    xt2 = 0
-    yt1 = 0
-    yt2 = 0
-    for (n, ci) in sheeps
-        i, j = Tuple(ci)
-        xt1 += n * cos( i / r)
-        yt1 += n * sin( i / r)
-
-        xt2 += n * cos( j / r)
-        yt2 += n * sin( j / r)
-    end
-
-    phi1 = if xt1 == 0 && yt1 == 0
-        2pi * rand()
-    else
-        atan(yt1, xt1)
-    end
-    if phi1 < 0
-        phi1 += 2pi
-    end
-
-    phi2 = if xt2 == 0 && yt2 == 0
-        2pi * rand()
-    else
-        atan(yt2, xt2)
-    end
-    if phi2 < 0
-        phi2 += 2pi
-    end
-    return (phi1 * world.boxsize[] / 2pi, phi2 * world.boxsize[] / 2pi)
-end
